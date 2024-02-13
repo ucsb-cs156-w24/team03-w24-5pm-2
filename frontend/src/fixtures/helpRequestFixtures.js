d09dec6a356029e48f6def1acb79b6352fee5cd2
Maria-Saucedo <msaucedo-flores@ucsb.edu>
2024-02-13 00:03:05 -0800

    M.S. - Added fixtures for HelpRequest

---
+++ frontend/src/fixtures/helpRequestFixtures.js
@@ -0,0 +1,51 @@
+const helpRequestFixtures = {
+    oneHelpRequest:
+    [
+      {
+        "id": 1,
+        "requesterEmail": "me@ucsb.edu",
+        "teamId": "5pm-2",
+        "tableOrBreakoutRoom": "1",
+        "requestTime": "2022-01-02T12:00:00",
+        "explanation": "help1",
+        "solved":"True"
+
+      }
+    ],
+
+    threeHelpRequests:
+    [
+        {
+            "id": 2,
+            "requesterEmail": "you@ucsb.edu",
+            "teamId": "5pm-1",
+            "tableOrBreakoutRoom": "2",
+            "requestTime": "2022-01-02T12:00:00",
+            "explanation": "help2",
+            "solved":"False"      
+        },
+
+        {
+            "id": 3,
+            "requesterEmail": "they@ucsb.edu",
+            "teamId": "4pm-3",
+            "tableOrBreakoutRoom": "3",
+            "requestTime": "2022-01-02T12:00:00",
+            "explanation": "help3",
+            "solved":"True"  
+        },
+
+        {
+            "id": 4,
+            "requesterEmail": "us@ucsb.edu",
+            "teamId": "4pm-2",
+            "tableOrBreakoutRoom": "4",
+            "requestTime": "2022-01-02T12:00:00",
+            "explanation": "help4",
+            "solved":"False" 
+        },
+        
+    ]
+};
+
+export { helpRequestFixtures };

d9f456413a378b5ad6588b4486830023ce565098
Jonathan Herring <jonathan_herring@ucsb.edu>
2024-02-15 14:55:04 -0800

    Add Menu Item Fixtures

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -0,0 +1,31 @@
+const ucsbDiningCommonsMenuItemFixtures = {
+    oneItem: {
+        "id": 1,
+        "diningCommonsCode": "carrillo",
+        "name": "Sliced Ham",
+        "station": "Deli"
+    },
+    threeDates: [
+        {
+            "id": 1,
+            "diningCommonsCode": "carrillo",
+            "name": "Sliced Ham",
+            "station": "Deli"
+        },
+        {
+            "id": 2,
+            "diningCommonsCode": "portola",
+            "name": "Vindaloo Chicken",
+            "station": "International"
+        },
+        {
+            "id": 3,
+            "diningCommonsCode": "ortega",
+            "name": "Baked Pesto Pasta with Chicken",
+            "station": "Entrees"
+        }
+    ]
+};
+
+
+export { ucsbDiningCommonsMenuItemFixtures };

1336a73869d7f079e4a7068c5f35a884606b5089
livitsimple <119549292+livitsimple@users.noreply.github.com>
2024-02-21 00:29:30 -0800

    Update UCSBOrganizationIndexPage.js

--- frontend/src/main/pages/UCSBOrganization/UCSBOrganizationIndexPage.js
+++ frontend/src/main/pages/UCSBOrganization/UCSBOrganizationIndexPage.js
@@ -1,15 +1,11 @@
 import React from 'react'
 import { useBackend } from 'main/utils/useBackend';
-
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 import UCSBOrganizationTable from 'main/components/UCSBOrganization/UCSBOrganizationTable';
 import { Button } from 'react-bootstrap';
 import { useCurrentUser , hasRole} from 'main/utils/currentUser';
-
 export default function UCSBOrganizationIndexPage() {
-
   const currentUser = useCurrentUser();
-
   const createButton = () => {
     if (hasRole(currentUser, "ROLE_ADMIN")) {
         return (
@@ -18,17 +14,18 @@ export default function UCSBOrganizationIndexPage() {
                 href="/ucsborganizations/create"
                 style={{ float: "right" }}
             >
-                Create Organization
+                Create Organization 
             </Button>
         )
-    }
+    } 
   }
-
+  
   const { data: organizations, error: _error, status: _status } =
     useBackend(
       // Stryker disable next-line all : don't test internal caching of React Query
       ["/api/ucsborganizations/all"],
       { method: "GET", url: "/api/ucsborganizations/all" },
+      // Stryker disable next-line all : don't test default value of empty list
       []
     );
 
